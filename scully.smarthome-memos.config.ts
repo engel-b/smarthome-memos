7fd5c36174df616125838b4d4b76a12bdc2e2b58
engel-b <contact.engel.b@gmail.com>
2020-10-25 11:34:01 +0100

    adding base-href-rewrite-plugin

--- scully.smarthome-memos.config.ts
+++ scully.smarthome-memos.config.ts
@@ -1,8 +1,13 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+
+const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
+setPluginConfig(baseHrefRewrite, { href: 'https://engel-b.github.io/smarthome-memos/' });
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "smarthome-memos",
   outDir: './dist/static',
+  defaultPostRenderers,
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
