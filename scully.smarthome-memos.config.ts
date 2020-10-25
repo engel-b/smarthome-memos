608e00259e70d280e83fffd83d64078975eca698
engel-b <contact.engel.b@gmail.com>
2020-10-25 12:53:35 +0100

    adding google analytics plugin

--- scully.smarthome-memos.config.ts
+++ scully.smarthome-memos.config.ts
@@ -1,7 +1,12 @@
-import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig, prod } from '@scullyio/scully';
+import { GoogleAnalytics } from '@scullyio/plugins/google-analytics';
 import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
 
 const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
+if (prod) {
+  setPluginConfig(GoogleAnalytics, { globalSiteTag: 'UA-116291357-1' });
+  defaultPostRenderers.push(GoogleAnalytics);
+}
 setPluginConfig(baseHrefRewrite, { href: 'https://engel-b.github.io/smarthome-memos/' });
 export const config: ScullyConfig = {
   projectRoot: "./src",
